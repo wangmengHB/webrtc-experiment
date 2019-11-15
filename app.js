77b4ff4f656c7bf09d9ceab8c0c5dd320f4ab670
meng.wm <meng.wm@alibaba-inc.com>
2019-11-15 09:41:11 +0800

    add ip hint

--- app.js
+++ app.js
@@ -118,6 +118,7 @@ var options = {
 
 https.createServer(options, app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
+  console.log(`https://${localIp}:${app.get('port')}`);
 });
 
 
